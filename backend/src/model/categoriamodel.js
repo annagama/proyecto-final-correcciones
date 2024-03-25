fcfb018f651e255f145deebc7542716156b952d8
annagama <142608511+annagama@users.noreply.github.com>
2024-03-25 08:15:16 -0300

    Create categoriamodel.js

---
+++ backend/src/model/categoriamodel.js
@@ -0,0 +1,13 @@
+const db=('..db\index.js');
+const{querytype}=require("sequelize");
+const getcategoria=()=>{
+    return db.query('select * from categoria',{
+        type:querytype.select
+    })
+}
+const getcategoriaid=(ncategoria)=>{
+    return db.query('select * from categoria ${ncategoria}',{
+        type:querytype.select
+    })
+}
+module.export={getcategoria,getcategoriaid}
