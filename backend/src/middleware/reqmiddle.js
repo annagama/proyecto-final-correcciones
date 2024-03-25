d09ad8f6f24f060991021b598101bf4f80334518
annagama <142608511+annagama@users.noreply.github.com>
2024-03-25 07:44:18 -0300

    Create reqmiddle.js

---
+++ backend/src/middleware/reqmiddle.js
@@ -0,0 +1,17 @@
+const productModel = require('..src\models\categoriamodel.js');
+
+const checkCantidadesProducto = async (req, res, next) => {
+
+    const {id_producto, ncantidad} = req.body;
+    
+    const product = await productmodel.getproductbyid(id_producto);
+    const cantidades = product[0].ncantidad;
+
+    if ( (cantidades - ncantidad )  >= 0) { 
+        next();
+    } else {
+        res.status(401).json({ message: 'No hay cantidades suficientes para añadir a la orden', success: false });
+    }
+};
+
+module.exports = {checkcantidadesproducto};
