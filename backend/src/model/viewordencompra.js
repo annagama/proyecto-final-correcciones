294adebcea68515ab5355f365eef4dfd3c24a1ac
annagama <142608511+annagama@users.noreply.github.com>
2024-03-25 09:10:00 -0300

    Create viewordencompra.js

---
+++ backend/src/model/viewordencompra.js
@@ -0,0 +1,27 @@
+const db = require("../db");
+const { querytypes } = require("sequelize");
+
+const getOrderUserInProcess = ( current_user) => {
+    return db.query( `SELECT * FROM orden_compra WHERE id_usuario = ${current_user} and bprocesado = false`,{
+      type: querytypes.SELECT,
+    });
+}
+
+
+const initorder = async (current_user) => {
+    const result = await db.query(
+        `INSERT INTO orden_compra (id_usuario) VALUES (${current_user})`
+    );
+    const orderid = result[0];
+    return orderid;
+};
+
+const updateorder = async ( id_orden_compra) => {
+    const result = await db.query(
+        `UPDATE orden_compra set bprocesado = 1, tsprocesado = NOW() where idorden_compra = ${id_orden_compra}`
+    );
+    
+    return result;
+};
+
+module.exports = { getorderuserinprocess, initorder, updateorder };
