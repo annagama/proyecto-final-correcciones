a668f1872406fb6da5385096af7bffde16f570f9
annagama <142608511+annagama@users.noreply.github.com>
2024-04-05 09:37:50 -0300

    Create pedido.js

---
+++ backend/src/router/pedido.js
@@ -0,0 +1,23 @@
+const { Router } = require('express');
+
+const pedidocontroller = require('');
+
+const { validarErrores } = require('..src\middlewares');
+const { idRequired } = require('..src/middlewares/paramsmiddleware.js');
+const { checkSession, checkSessionAdmin } = require('.src');
+
+const pedidorouter = Router();
+
+pedidorouter.get("/getAll", pedidocontroller.getAll);
+
+pedidorouter.get("/getPedidosSinProcesar", pedidocontroller.getPedidosSinProcesar);
+
+pedidorouter.post("/generarPedido", [checkSession, validarErrores], pedidocontroller.generarPedido);
+
+pedidorouter.post("/procesarPedido/:id", [checkSessionAdmin, idRequired, validarErrores], pedidocontroller.procesarPedido);
+
+pedidorouter.get("/obtenerPedidosPorUsuario", [checkSession, validarErrores], pedidocontroller.getPedidosUsuario);
+
+pedidorouter.get("/notificacionProcesada", [checkSession, validarErrores], pedidocontroller.procesarNotificacion);
+
+module.exports = pedidorouter;
