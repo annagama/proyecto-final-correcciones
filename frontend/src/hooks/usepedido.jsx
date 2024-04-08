06b83eabe18c1709b5a0b2e6a8b24809396a9f8d
annagama <142608511+annagama@users.noreply.github.com>
2024-04-08 08:06:17 -0300

    Create usepedido.jsx

---
+++ frontend/src/hooks/usepedido.jsx
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+export const usePedidos = () => {
+    const [pedidosData, setPedidos] = useState([]);
+    
+    useEffect(() => {
+        cargarPedidos();
+    }, [])
+    
+    const cargarPedidos = async () => {
+      const resp = await axios.get('/pedido/getAll')
+      const product = await resp.data;
+      setPedidos(product.data)
+    }
+
+   
+
+  
+
+   
+    return {
+        pedidosData,
+        cargarPedidos     
+    }
+}
