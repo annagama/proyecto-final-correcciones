6eee5b999b4b6ce03c751bcab8fed79c2adb2fe8
annagama <142608511+annagama@users.noreply.github.com>
2024-04-08 08:25:34 -0300

    Update usepedido.jsx

--- frontend/src/hooks/usepedido.jsx
+++ frontend/src/hooks/usepedido.jsx
@@ -9,7 +9,7 @@ export const usePedidos = () => {
     }, [])
     
     const cargarPedidos = async () => {
-      const resp = await axios.get('/pedido/getAll')
+      const resp = await axios.get('/pedido/getall')
       const product = await resp.data;
       setPedidos(product.data)
     }
