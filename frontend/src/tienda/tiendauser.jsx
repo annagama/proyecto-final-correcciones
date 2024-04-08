a2852a7ef4add93321f0c938345019552f42e090
annagama <142608511+annagama@users.noreply.github.com>
2024-04-08 07:43:21 -0300

    Update tiendauser.jsx

--- frontend/src/tienda/tiendauser.jsx
+++ frontend/src/tienda/tiendauser.jsx
@@ -1,1 +1,58 @@
+import { create } from "zustand";
+import axios from "axios";
+import {errorModal, confirmMessage} from '../Utilities/Utilities';
 
+export const useCartStore = create((set) => ({
+  cart: [],
+  cantidad: 0,
+  precioTotal: 0,
+  addProduct: async (vObj) => {
+    try {
+     
+      const response = await axios.post("order/addProductToCart",  vObj );
+      
+      set((state) => ({
+        cart: response.data.productos, 
+        cantidad: response.data.cantidad,
+        precioTotal: response.data.precioTotal
+      }));
+    } catch (error) { 
+      errorModal(error.response.data.msg)
+    }
+  },
+  getProduct: async () => {
+    try {
+      
+      const response = await axios.get("order/getUserOrder");
+
+      set((state) => ({
+        cart: response.data.productos, 
+        cantidad: response.data.cantidad,
+        precioTotal: response.data.precioTotal
+      }));
+    } catch (error) {     
+      errorModal(error.response.data.msg)
+    }
+  },
+  resetCart: () => {
+    set({
+      cart: [],
+      cantidad: 0,
+    });
+  },
+  deleteProduct: async (vObj) => {
+    try {
+  
+      const response = await axios.post("order/deleteProduct", vObj);
+
+      // Actualizar el estado con el carrito actualizado
+      set((state) => ({
+        cart: response.data.productos, 
+        cantidad: response.data.cantidad,
+        precioTotal: response.data.precioTotal
+      }));
+    } catch (error) {
+      console.error("Error al eliminar el producto del carrito", error);
+      errorModal(error.response.data.msg)
+    }
+  },
