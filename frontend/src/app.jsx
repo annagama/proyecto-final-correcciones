834635c98191be5edd2d615d1f6bcfa64e521df6
annagama <142608511+annagama@users.noreply.github.com>
2024-04-08 08:21:14 -0300

    Create app.jsx

---
+++ frontend/src/app.jsx
@@ -0,0 +1,145 @@
+import React from 'react';
+import Header from './componente/headyfooter/head.jsx';
+import Footer from './componente/headyfooter/footer.jsx';
+import './app.css';
+import LogIn from './componente/login/login.jsx';
+import SignUp from './componente/login/singin.jsx';
+import Cart from './componente/cart/cart.jsx';
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import DetailProduct from './componente/producto/detalleproduct.jsx';
+import { useUserStore } from './tienda/user-tienda.jsx';
+import Pedidos from './componente/pedido/pedido.jsx';
+import ErrorPage from './componente/errorpag/error.jsx';
+import UserCompra from './componente/compra/usercompra.jsx';
+
+
+function App() {
+
+  const current_user = useUserStore((state) => state);
+  
+  const RedirectToHome = () => <Navigate to="/" replace={true} />;
+
+  console.log(current_user);
+  
+  return (
+    <BrowserRouter>
+      <>
+        <Header />
+
+        <Routes>         
+          <Route path="/" element={<ProductList />} />
+          
+          {current_user.isLogged ? (
+            <Route
+              path="/login"
+              element={<RedirectToHome />}
+            />
+          ) : (
+            <Route path="/login" element={<LogIn />} />
+          )}
+
+          {current_user.isLogged ? (
+            <Route
+              path="/signup"
+              element={<RedirectToHome />}
+            />
+          ) : (
+            <Route path="/signup" element={<SignUp />} />
+          )}
+
+               
+          
+          <Route
+            path="/carrito"
+            element={
+              current_user.isLogged ? (
+                current_user.usuario.administrador ? (
+                  <ErrorPage status={403} message={'Atención!! no posee acceso a esta pagina'}/>
+                ) : (
+                  <Cart />
+                )
+              ) : (
+                <LogIn />
+              )
+            }
+          />
+
+          <Route
+            path="/favorito"
+            element={
+              current_user.isLogged ? (
+                current_user.usuario.administrador ? (
+                  <ErrorPage status={403} message={'Atención!! no posee acceso a esta pagina'}/>
+                ) : (
+                  <Favorito />
+                )
+              ) : (
+                <LogIn />
+              )
+            }
+          />
+
+          <Route
+            path="/tuscompras"
+            element={
+              current_user.isLogged ? (
+                current_user.usuario.administrador ? (
+                  <ErrorPage status={403} message={'Atención!! no posee acceso a esta pagina'}/>
+                ) : (
+                  <UserCompra />
+                )
+              ) : (
+                <LogIn />
+              )
+            }
+          />
+
+
+          <Route
+            path="/backproduct"
+            element={
+              current_user.isLogged && current_user.usuario.administrador ? (
+                <ProductBackOffice />
+              ) : (
+                <ErrorPage status={403} message={'Atención!! no posee acceso a esta pagina'}/>
+              )
+            }
+          />
+
+            
+          <Route
+            path="/nuevoproducto"
+            element={
+              current_user.isLogged && current_user.usuario.administrador ? (
+                <NuevoProducto />
+              ) : (
+                <ErrorPage status={403} message={'Atención!! no posee acceso a esta pagina'}/>
+              )
+            }
+          />
+
+
+          
+          <Route
+            path="/pedidos"
+            element={
+              current_user.isLogged && current_user.usuario.administrador ? (
+                <Pedidos />
+              ) : (
+                <ErrorPage status={403} message={'Atención!! no posee acceso a esta pagina'}/>
+              )
+            }
+          />
+
+          <Route path="/detail/:id" element={<DetailProduct />} />
+
+          <Route path="*" element={<ErrorPage status={404} message={'La pagina no existe'} />} />
+        </Routes>
+
+        <Footer />
+      </>
+    </BrowserRouter>
+  );
+}
+
+export default App;
