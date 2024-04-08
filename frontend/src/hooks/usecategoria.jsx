0b5befc195f749951afff4f0651f4c2f17cf94ea
annagama <142608511+annagama@users.noreply.github.com>
2024-04-08 07:59:03 -0300

    Update usecategoria.jsx

--- frontend/src/hooks/usecategoria.jsx
+++ frontend/src/hooks/usecategoria.jsx
@@ -1,1 +1,20 @@
-
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+export const useCategory = () => {
+    const [categorias, setCategoria] = useState([]);
+    
+    
+    useEffect(() => {
+        loadCategory();
+    }, [])
+    
+    const loadCategory = async () => {
+        const resp = await axios.get('')
+        const category = await resp.data;
+        setCategoria(category.data)
+    }
+   
+    return {
+        categorias      
+    }
+}
