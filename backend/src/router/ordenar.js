6e8d344ae1280257cefcb4214395b9ba4f9a8427
annagama <142608511+annagama@users.noreply.github.com>
2024-04-19 09:49:58 -0300

    Update ordenar.js

--- backend/src/router/ordenar.js
+++ backend/src/router/ordenar.js
@@ -1,10 +1,10 @@
 const { Router } = require("express");
-const OrdenCompraController = require('..src\controllers\ordenar.js');
-const PedidoController = require('..src\controllers\pedido.js');
+const OrdenCompraController = require('..src\controllers\ordenar');
+const PedidoController = require('..src\controllers\pedido');
 const OrderRouter = Router();
 const { validarErrores } = require('..src\middlewares');
 const { checkSession, checkSessionAdmin } = require('..src');
-const { idRequired } = require('..src/middlewares/paramsmiddleware.js');
+const { idRequired } = require('..src/middlewares/paramsmiddleware');
 OrderRouter.post("/addproductcarrito", [checkSession, validarErrores], ordencompracontroller.addcarrito);
 
 OrderRouter.post("/deleteproduct", [checkSession, validarErrores], ordencompracontroller.deleteproductcarrito);
