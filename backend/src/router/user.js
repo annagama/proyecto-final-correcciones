f9ad37118dfc4a04ac2e6f380e0e0832541b0a57
annagama <142608511+annagama@users.noreply.github.com>
2024-04-19 09:59:59 -0300

    Update user.js

--- backend/src/router/user.js
+++ backend/src/router/user.js
@@ -1,11 +1,10 @@
 const { Router } = require('express');
 
-const usercontroller = require('..src\controllers\user.js');
+const usercontroller = require('..src\controllers\user');
 const UserRouter = Router();
 
 const { validarErrores } = require('..src/middlewares');
-const { validatenome, validateapellido, validatedireccion, validateemail, validatePassword } = require('..src\middlewares\usermiddle.js');
-
+const { validatenome, validateapellido, validatedireccion, validateemail, validatePassword } = require('..src\middlewares\usermiddle');
 UserRouter.post("/signUp", [validatenome, validateapellido, validatedireccion, validateemail, validatepassword, validarerrores], usercontroller.adduser);
 
 UserRouter.post("/logIn", [validateemail, validatepassword, validarerrores], usercontroller.logIn);
